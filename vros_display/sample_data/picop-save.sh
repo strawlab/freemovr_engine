d17d89f84ca27c88fdd0e3251ff307c259ca8d5c
Andrew Straw <strawman@astraw.com>
2012-01-07 08:02:48 +0100

    add some sample display configs

---
+++ vros_display/sample_data/picop-save.sh
@@ -0,0 +1,2 @@
+rosparam dump picop/vdisp_mirror.yaml /virtual_displays/picop/vdisp_mirror
+rosparam dump picop/vdisp.yaml /virtual_displays/picop/vdisp
---
+++ vros_display/sample_data/picop.sh
@@ -0,0 +1,3 @@
+rosparam load picop/picop.yaml physical_displays/picop
+rosparam load picop/vdisp.yaml virtual_displays/picop/vdisp
+rosparam load picop/vdisp_mirror.yaml virtual_displays/picop/vdisp_mirror
