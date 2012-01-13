1e6d93f68addd95caa09d4e11600c0f02fabd736
Andrew Straw <strawman@astraw.com>
2012-01-13 08:23:31 -0500

    fix beachball

--- vros_display/src/draw_beachball.py
+++ vros_display/src/draw_beachball.py
@@ -6,14 +6,17 @@ el = np.linspace(-90, 90, 512)
 
 im = np.zeros( (512,512,3), dtype=np.uint8)
 
+# red channel = azimuth
 im[:, (  0 <= az) & (az <  90), 0] = 0
 im[:, ( 90 <= az) & (az < 180), 0] = .333*255
 im[:, (180 <= az) & (az < 270), 0] = .666*255
 im[:, (270 <= az) & (az < 360), 0] = 1.0*255
 
+# green channel = elevation
 im[(-90<=el) & (el<  0),:,1] = 0
 im[(  0<=el) & (el< 90),:,1] = .5*255
 
+# blue channel = x coord
 im[:, ( 90 <= az) & (az < 270), 2] = 1*255
 
-scipy.misc.imsave('beachball.png',im)
+scipy.misc.imsave('beachball.png',im[::-1])
