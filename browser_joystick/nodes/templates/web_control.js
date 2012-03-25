53ee0d46b2c2da50c9deffb6b0d15ce4ab63430a
Andrew Straw <strawman@astraw.com>
2012-03-25 18:10:06 +0200

    add buttons to joystick simulation

--- browser_joystick/nodes/templates/web_control.js
+++ browser_joystick/nodes/templates/web_control.js
@@ -30,17 +30,18 @@ function init()
 window.addEventListener("load", init, false);
 
 window.onload = function() {
-    var stage = new Kinetic.Stage("container", 200, 200);
-    big_radius = Math.min(stage.width, stage.height)/2 - 4;
+    var stage = new Kinetic.Stage("container", 400, 200);
+    big_radius = 200/2-4;
+    button_state = [0, 0, 0, 0];
 
     var joyLayer = new Kinetic.Layer();
     var bgLayer = new Kinetic.Layer();
-    rectX = stage.width / 2;
-    rectY = stage.height / 2;
+    joyCenterX = 100;
+    joyCenterY = 100;
 
     joytip = new Kinetic.Circle({
-        x: rectX,
-        y: rectY,
+        x: joyCenterX,
+        y: joyCenterY,
         radius: 15,
         fill: "#00D2FF",
         stroke: "black",
@@ -48,25 +49,20 @@ window.onload = function() {
         draggable: true
     });
 
-    // add cursor styling
-    joytip.on("mouseover", function(){
-        document.body.style.cursor = "pointer";
-    });
-    joytip.on("mouseout", function(){
-        document.body.style.cursor = "default";
-    });
+    cursor_style(joytip);
+
 
     joytip.on("mouseup touchend", function() {
-        joytip.x = rectX;
-        joytip.y = rectY;
+        joytip.x = joyCenterX;
+        joytip.y = joyCenterY;
         joyLayer.draw();
         emit_joy_pos();
     });
 
     function emit_joy_pos() {
         // slightly funky scaling to match ROS joystick node
-        axes = [-(joytip.x-rectX)/big_radius,
-                -(joytip.y-rectY)/big_radius]
+        axes = [-(joytip.x-joyCenterX)/big_radius,
+                -(joytip.y-joyCenterY)/big_radius]
 
         var ms = "joystick axes: " + axes;
         $("#output").empty();
@@ -74,7 +70,7 @@ window.onload = function() {
         ws.send(JSON.stringify({
             msg: "joy",
             axes: axes,
-            buttons: []
+            buttons: button_state
         }));
     }
 
@@ -85,8 +81,8 @@ window.onload = function() {
     joyLayer.add(joytip);
 
     var bg = new Kinetic.Circle({
-        x: rectX,
-        y: rectY,
+        x: joyCenterX,
+        y: joyCenterY,
         radius: big_radius,
         stroke: "black",
         strokeWidth: 2,
@@ -95,6 +91,84 @@ window.onload = function() {
 
     stage.add(bgLayer);
     stage.add(joyLayer);
+
+    add_buttons();
+
+    function add_buttons() {
+        var buttonLayer = new Kinetic.Layer();
+
+        var allBtnCenterX = 300;
+        var allBtnCenterY = 100;
+        var btnOffset = 50;
+
+        var cfg = { num: 0,
+                    x: allBtnCenterX-btnOffset,
+                    y: allBtnCenterY,
+                    color: "red"
+                  }
+        add_button(cfg, buttonLayer);
+
+        var cfg = { num: 1,
+                    x: allBtnCenterX,
+                    y: allBtnCenterY+btnOffset,
+                    color: "yellow"
+                  }
+        add_button(cfg, buttonLayer);
+
+        var cfg = { num: 2,
+                    x: allBtnCenterX+btnOffset,
+                    y: allBtnCenterY,
+                    color: "green"
+                  }
+        add_button(cfg, buttonLayer);
+
+        var cfg = { num: 3,
+                    x: allBtnCenterX,
+                    y: allBtnCenterY-btnOffset,
+                    color: "blue"
+                  }
+        add_button(cfg, buttonLayer);
+
+        stage.add(buttonLayer);
+    }
+
+    function add_button(cfg, buttonLayer) {
+        var btnRadius = 20;
+        var btn = new Kinetic.Circle({
+            x: cfg.x,
+            y: cfg.y,
+            radius: btnRadius,
+            fill: cfg.color,
+            stroke: "black",
+            strokeWidth: 2,
+        });
+        buttonLayer.add(btn);
+        cursor_style(btn);
+
+        btn.on("mousedown touchstart", function() {
+            button_state[cfg.num] = 1;
+            btn.fill = "black";
+            buttonLayer.draw();
+            emit_joy_pos();
+        });
+
+        btn.on("mouseup touchend", function() {
+            button_state[cfg.num] = 0;
+            btn.fill = cfg.color;
+            buttonLayer.draw();
+            emit_joy_pos();
+        });
+    }
+
+    function cursor_style( node ) {
+        // add cursor styling
+        node.on("mouseover", function(){
+            document.body.style.cursor = "pointer";
+        });
+        node.on("mouseout", function(){
+            document.body.style.cursor = "default";
+        });
+    }
 };
 
 
