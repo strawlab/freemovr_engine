399bb5f15148ac8e1f3eca350a92937f4d0c9bb0
Andrew Straw <strawman@astraw.com>
2012-03-30 15:04:47 +0200

    add bare-bones stimulus template

---
+++ vros_display/src/StimulusTemplate.cpp
@@ -0,0 +1,48 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+
+/* StimulusTemplate
+
+   This is a completely empty, do-nothing stimulus that is useful for
+   using as a starting point for writing other plugins.
+
+ */
+
+#include "vros_display/stimulus_interface.h"
+#include "Poco/ClassLibrary.h"
+#include <stdexcept>
+
+class StimulusTemplate: public StimulusInterface
+{
+public:
+StimulusTemplate() {
+}
+
+std::string name() const {
+    return "StimulusTemplate";
+}
+
+std::vector<std::string> get_topic_names() const {
+	std::vector<std::string> result;
+	return result;
+}
+
+std::string get_message_type(const std::string& topic_name) const {
+	throw std::runtime_error("no message types to get");
+}
+
+void receive_json_message(const std::string& topic_name, const std::string& json_message) {
+}
+};
+
+POCO_BEGIN_MANIFEST(StimulusInterface)
+POCO_EXPORT_CLASS(StimulusTemplate)
+POCO_END_MANIFEST
+
+// optional set up and clean up functions
+void pocoInitializeLibrary()
+{
+}
+
+void pocoUninitializeLibrary()
+{
+}
