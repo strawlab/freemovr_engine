868f8f73b9de9487f9d06fce2a8ad1de7570fe3a
John Stowers <john.stowers@gmail.com>
2012-05-24 22:07:57 +0200

    tests for mcsc

--- vros_display/test/testmcsc.py
+++ vros_display/test/testmcsc.py
@@ -3,21 +3,22 @@ roslib.load_manifest('vros_display')
 from calib.io import MultiCalSelfCam, load_ascii_matrix
 import rospy
 
-import numpy as np
+import sys
 import pickle
+
+import numpy as np
+
+d = '/home/stowers/vros-devel/vros/mcamall/'
 
 rospy.init_node('testmcsc', anonymous=True)
 
-if 0:
-    mcsc = MultiCalSelfCam('/home/stowers/Straw/MultiCamSelfCal/strawlab/test-data/DATA20100906_134124/')
-    mcsc.publish_calibration_points()
-    mcsc.save_to_pcd('/tmp/test.pcd')
-    rospy.spin()
+mcsc = MultiCalSelfCam(d)
+mcsc.publish_calibration_points()
+mcsc.save_to_pcd('/tmp/test.pcd')
+rospy.spin()
 
-    idp = load_ascii_matrix(
-            '/home/stowers/Straw/MultiCamSelfCal/strawlab/test-data/DATA20100906_134124/IdMat.dat')
-    pts = load_ascii_matrix(
-            '/home/stowers/Straw/MultiCamSelfCal/strawlab/test-data/DATA20100906_134124/points.dat')
+idp = load_ascii_matrix(d+'/IdMat.dat')
+pts = load_ascii_matrix(d+'/points.dat')
 
 
 mcsc = MultiCalSelfCam('allcalibresults')
