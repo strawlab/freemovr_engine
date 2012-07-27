bc089c6b2bc7d44980442b874fdaf398b34b4d54
Andrew Straw <strawman@astraw.com>
2012-07-27 10:42:16 +0200

    allow loading PointCloudTransformer from .json file

--- vros_display/src/calib/reconstruct.py
+++ vros_display/src/calib/reconstruct.py
@@ -51,6 +51,23 @@ class PointCloudTransformer(object):
         with open(fname,mode='w') as fd:
             fd.write(buf)
 
+    @staticmethod
+    def from_json(fname):
+        with open(fname,mode='r') as fd:
+            alignment = json.loads(fd.read())
+
+        s=alignment['s']
+
+        t=alignment['t']
+        t = np.array(t)
+        t.shape = (1,3)
+
+        R=alignment['R']
+        R = np.array(R)
+
+        recon = PointCloudTransformer(scale=s, translate=t, rotate=R)
+        return recon
+
     def move_cloud(self, arr):
         #do it lazily because arr is not homogenous coords
         new = np.dot(self._s * self._R,arr.T).T
@@ -83,7 +100,7 @@ class CylinderPointCloudTransformer(PointCloudTransformer):
         rotation_axis = np.cross(axis, (0, 0, 1))
         rotation_angle = simple_geom.angle_between_vectors((0, 0, 1), axis)
         rotation_quaternion = tf.transformations.quaternion_about_axis(rotation_angle, axis)
-        
+
         print "FIXING SCALE " * 5
         self._s = 1.0/radius
 
@@ -112,7 +129,7 @@ class CylinderPointCloudTransformer(PointCloudTransformer):
 
     def get_geom_dict(self):
         return self._cyl.to_geom_dict()
-        
+
 def interpolate_pixel_cords(points_2d, values_1d, img_width, img_height, method='cubic', fill_value=np.nan):
     assert points_2d.ndim == 2
     assert values_1d.ndim == 1
@@ -127,5 +144,5 @@ def interpolate_pixel_cords(points_2d, values_1d, img_width, img_height, method=
                 fill_value=fill_value)
 
     return res
-        
+
 
