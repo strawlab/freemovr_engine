8c4f2cfd99f132372c9286854caef48ba51a0672
Andrew Straw <strawman@astraw.com>
2012-07-27 10:42:41 +0200

    script for aligning inlier points

---
+++ vros_display/scripts/align-inliers.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import argparse, json, os
+
+import roslib;
+roslib.load_manifest('vros_display')
+
+from calib.io import MultiCalSelfCam
+from calib.reconstruct import PointCloudTransformer
+
+def align_inliers( align_json, in_dir, out_dir ):
+    recon = PointCloudTransformer.from_json( align_json )
+    Xe, Ce, Re = MultiCalSelfCam.read_calibration_result(in_dir)
+
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
+    MultiCalSelfCam.transform_and_save( Xe, Ce, Re, out_dir, recon )
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--align-json', type=str)
+    parser.add_argument(
+        '--in-dir', type=str)
+    parser.add_argument(
+        '--out-dir', type=str)
+    args = parser.parse_args()
+
+    align_inliers( args.align_json, args.in_dir, args.out_dir )
