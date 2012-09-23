dce066b92d052fa04c80f16c82fc23c28678a897
Andrew Straw <strawman@astraw.com>
2012-09-23 17:36:13 +0200

    vros_display: print screen resolution

--- vros_display/nodes/show_screens.py
+++ vros_display/nodes/show_screens.py
@@ -1,12 +1,40 @@
-from gi.repository import Gtk, Gdk
+#!/usr/bin/env python
+
+import roslib; roslib.load_manifest('flycave')
+import rospy
+
+import threading
+
+from gi.repository import Gtk, GObject, Gdk, GLib
+
+GObject.threads_init()
+Gdk.threads_init()
 
 COLORS = ("red","green","blue","magenta","yellow","teal","brown")
 
+def check_shutdown(rosthread):
+    alive = rosthread.is_alive()
+    if not alive:
+        Gtk.main_quit()
+        return False
+    return True
+
 class WindowOnScreen:
-    def __init__(self, label, color, screen):
+    def __init__(self, color, screen, display):
         w = Gtk.Window()
+        w.connect("delete-event", Gtk.main_quit)
         b = Gtk.EventBox()
         l = Gtk.Label()
+
+        width = screen.get_width()
+        height = screen.get_height()
+
+        label = 'Display %s\nScreen %d\n%d x %d' % (
+            display.get_name(),
+            screen.get_number(),
+            width,
+            height)
+
         l.set_markup('<span size="xx-large">%s</span>' % label)
         b.add(l)
 
@@ -19,16 +47,30 @@ class WindowOnScreen:
         l.override_color(Gtk.StateFlags.NORMAL,fg)
 
         w.add(b)
-        w.set_default_size (screen.get_width(), screen.get_height())
+        w.set_default_size (width, height)
         w.fullscreen()
         w.set_screen(screen)
         w.show_all()
 
 if __name__ == "__main__":
+    rospy.init_node("show_screens", anonymous=True, disable_signals=True )
+
+    rosthread = threading.Thread(name="ros spin thread", target=rospy.spin)
+    rosthread.daemon = True
+    rosthread.start()
+
+    GLib.timeout_add(1000/10, check_shutdown, rosthread)
+
     d = Gdk.Display.get_default()
-    for i in range(0, d.get_n_screens()):
+    nscreens = d.get_n_screens()
+
+    rospy.loginfo("Display %s has %d screens" % (d.get_name(), nscreens))
+
+    for i in range(0, nscreens):
         WindowOnScreen(
-            'Display %s Screen: %d' % (d.get_name(),i),
-            COLORS[i],
-            d.get_screen(i))
+            COLORS[i % len(COLORS)],
+            d.get_screen(i),
+            d)
     Gtk.main()
+
+    rospy.signal_shutdown('quit from gui')
