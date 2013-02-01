4666980487b95cc3bde0579a7c24652cf084d83b
Andrew Straw <strawman@astraw.com>
2013-02-01 23:41:45 +0100

    add test

---
+++ test/test_rosmsg2json.py
@@ -0,0 +1,14 @@
+import json
+
+ROS_PACKAGE_NAME='flyvr'
+import roslib; roslib.load_manifest(ROS_PACKAGE_NAME)
+
+import sensor_msgs.msg
+
+import rosmsg2json
+
+def test_arrays():
+    msg = sensor_msgs.msg.CameraInfo()
+    d = rosmsg2json.rosmsg2dict(msg)
+    json.dumps(d)
+
