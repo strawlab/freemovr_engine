9b6356a601c070f0af1e63a28408c15429d50009
John Stowers <john.stowers@gmail.com>
2013-02-20 15:42:05 +0100

    calib: implement a spiral search iterator

--- src/calib/sampling.py
+++ src/calib/sampling.py
@@ -36,11 +36,41 @@ def gen_vert_snake(w,h,sw,sh=None,linspace=False,startw=0,starth=0):
                 yield ww,hh
             reverse = 1
 
+def gen_spiral_snake(w, h, startw=0, starth=0):
+    x,y = 0,0   
+    dx, dy = 0, -1
+
+    for dumb in xrange(w*h):
+        if abs(x) == abs(y) and [dx,dy] != [1,0] or x>0 and y == 1-x:  
+            dx, dy = -dy, dx            # corner, change direction
+
+        if abs(x)>w/2 or abs(y)>h/2:    # non-square
+            dx, dy = -dy, dx            # change direction
+            x, y = -y+dx, x+dy          # jump
+
+        yield x+startw, y+starth
+        x, y = x+dx, y+dy
+
 if __name__ == "__main__":
-    print tuple(gen_horiz_snake(15,10,3,2))
-    print
-    print tuple(gen_horiz_snake(15,10,3))
-    print
-    print tuple(gen_horiz_snake(15,10,3,2,linspace=True))
-    print 
-    print tuple(gen_vert_snake(15,10,3))
+    import matplotlib.pyplot as plt
+
+    def do_plt(gen):
+        plt.figure()
+        x,y = [],[]
+        for _x,_y in gen:
+            x.append(_x)
+            y.append(_y)
+        print len(x)
+        plt.plot(x,y)
+        plt.xlim(-5,20)
+        plt.ylim(-5,20)
+
+    do_plt(gen_horiz_snake(15,10,3,2))
+    do_plt(gen_horiz_snake(15,10,1,1,startw=-2,starth=-4))
+    do_plt(gen_horiz_snake(15,10,3,2,linspace=True))
+    do_plt(gen_vert_snake(15,10,3))
+    do_plt(gen_spiral_snake(12,5))
+    do_plt(gen_spiral_snake(5,5,startw=5,starth=5))
+    do_plt(gen_spiral_snake(6,6,startw=4,starth=4))
+
+    plt.show()
