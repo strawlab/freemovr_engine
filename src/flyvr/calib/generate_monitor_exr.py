29b8fd29d49cda4d62458a122f81f20227781e71
Andrew Straw <strawman@astraw.com>
2013-03-08 11:12:31 +0100

    generate_monitor_exr.py: new: generate very simple EXR file for flat panels

---
+++ src/flyvr/calib/generate_monitor_exr.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- Mode: python; tab-width: 4; indent-tabs-mode: nil -*-
+import argparse
+import numpy as np
+
+import roslib; roslib.load_manifest('flyvr')
+from exr import save_exr
+
+def gen_exr(fname=None, width=None, height=None):
+    u = np.linspace( 0.0, 1.0, width )
+    v = np.linspace( 0.0, 1.0, height )
+
+    U,V = np.meshgrid(u,v)
+    B = -1*np.ones_like(U)
+    save_exr( fname, r=U, g=V, b=B)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--width', type=int, default=1920,
+                        help="width of display (in pixels)")
+    parser.add_argument('--height', type=int, default=1080,
+                        help="width of display (in pixels)")
+    parser.add_argument('--fname', type=str, default='monitor.exr',
+                        help="name of generated EXR file")
+    args = parser.parse_args()
+
+    gen_exr( fname=args.fname, width=args.width, height=args.height )
+
