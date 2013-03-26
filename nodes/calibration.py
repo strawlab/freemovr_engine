328a4b6eeab7a6a9609e765612cf14e35a5b6776
Andrew Straw <strawman@astraw.com>
2013-03-26 11:47:39 +0100

    display_client: hide the sprite

--- nodes/calibration.py
+++ nodes/calibration.py
@@ -263,8 +263,9 @@ class Calib:
     def __init__(self, config, show_cameras, show_display_servers, show_type, outdir, continue_calibration, calibration_except, enable_mouse_click, debug):
         tracking_cameras = config["tracking_cameras"]
         laser_camera = config["laser_camera"]
-        trigger = config["trigger"]
         laser = config["laser"]
+
+        trigger = '/camera_trigger'
 
         if 0:
             ####SAVE EVERY IMAGE FOR NICE MOVIE
@@ -415,6 +416,8 @@ class Calib:
         rospy.loginfo("Connecting to cam %s" % laser_camera)
         self.laser_detector = fd
         self.laser_mask = load_mask_image(decode_url(config["laser_camera_mask"]))
+
+        display_client.DisplayServerProxy.hide_sprite()
 
         #need to recieve images (i.e. by calculating the background) before
         #being able to read the resoluions
@@ -1274,7 +1277,7 @@ if __name__ == '__main__':
     conffile = decode_url(args.calib_config)
     with open(conffile, 'r') as f:
         config = yaml.load(f)
-        for k in ("tracking_cameras", "display_servers", "trigger", "mask_dir"):
+        for k in ("tracking_cameras", "display_servers", "mask_dir"):
             if not config.has_key(k):
                 parser.error("malformed calibration config, missing %s" % k)
 
