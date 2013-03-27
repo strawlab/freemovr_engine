f3680a75e23c227df43294f9cae6707e4e4d1280
Andrew Straw <strawman@astraw.com>
2013-03-27 18:41:49 +0100

    fix namespace: remove rosmsg2json backwards compatibility

--- src/rosmsg2json.py
+++
@@ -1,9 +0,0 @@
-# This module will be deleted. It is for backwards compatibility. DO NOT EDIT.
-from flyvr.rosmsg2json import *
-import warnings
-import os
-
-if int(os.environ.get('FLYVR_THROW_DEPRECATION','0')):
-    raise RuntimeError('you are importing a deprecated module')
-
-warnings.warn("You are importing module 'rosmsg2json'. Please update this to module 'flyvr.rosmsg2json'")
--- test/test_rosmsg2json.py
+++ test/test_rosmsg2json.py
@@ -5,7 +5,7 @@ import roslib; roslib.load_manifest(ROS_PACKAGE_NAME)
 
 import sensor_msgs.msg
 
-import rosmsg2json
+import flyvr.rosmsg2json as rosmsg2json
 
 def test_arrays():
     msg = sensor_msgs.msg.CameraInfo()
