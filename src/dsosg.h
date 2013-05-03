e29b07fbf8834ec70f96b5998469d70e409beccc
Andrew Straw <strawman@astraw.com>
2013-05-03 18:38:55 +0200

    dsosg.h: untabify

--- src/dsosg.h
+++ src/dsosg.h
@@ -22,91 +22,91 @@
 
 namespace dsosg{
 
-	typedef struct {
-		osg::Quat rotation;
-		osg::Vec3 center;
-		double distance;
-	} TrackballManipulatorState;
+    typedef struct {
+        osg::Quat rotation;
+        osg::Vec3 center;
+        double distance;
+    } TrackballManipulatorState;
 
-	typedef Poco::ClassLoader<StimulusInterface> StimulusLoader;
+    typedef Poco::ClassLoader<StimulusInterface> StimulusLoader;
 
-	class ObserverPositionCallback: public osg::Uniform::Callback {
-	public:
-		ObserverPositionCallback() {}
-		virtual void operator() ( osg::Uniform* uniform, osg::NodeVisitor* nv );
-		virtual void setObserverPosition( osg::Vec3 p );
-	private:
-		OpenThreads::Mutex      _mutex;
-		osg::Vec3 _p;
-	};
+    class ObserverPositionCallback: public osg::Uniform::Callback {
+    public:
+        ObserverPositionCallback() {}
+        virtual void operator() ( osg::Uniform* uniform, osg::NodeVisitor* nv );
+        virtual void setObserverPosition( osg::Vec3 p );
+    private:
+        OpenThreads::Mutex      _mutex;
+        osg::Vec3 _p;
+    };
 
-	class CameraCube; // forward declaration
+    class CameraCube; // forward declaration
 
-	class DSOSG {
-	public:
-		DSOSG(std::string flyvr_basepath, std::string mode, float observer_radius, std::string config_fname, bool two_pass=false,
-			  bool show_geom_coords=false, bool tethered_mode=true, bool slave=false);
+    class DSOSG {
+    public:
+        DSOSG(std::string flyvr_basepath, std::string mode, float observer_radius, std::string config_fname, bool two_pass=false,
+              bool show_geom_coords=false, bool tethered_mode=true, bool slave=false);
 
-		std::vector<std::string> get_stimulus_plugin_names();
-		std::string get_current_stimulus_plugin_name() { return std::string(_current_stimulus->name()); }
-		void set_stimulus_plugin(const std::string& plugin_name);
+        std::vector<std::string> get_stimulus_plugin_names();
+        std::string get_current_stimulus_plugin_name() { return std::string(_current_stimulus->name()); }
+        void set_stimulus_plugin(const std::string& plugin_name);
 
-		std::vector<std::string> stimulus_get_topic_names(const std::string& plugin_name);
-		std::string stimulus_get_message_type(const std::string& plugin_name, const std::string& topic_name);
-		void stimulus_receive_json_message(const std::string& plugin_name, const std::string& topic_name, const std::string& json_message);
+        std::vector<std::string> stimulus_get_topic_names(const std::string& plugin_name);
+        std::string stimulus_get_message_type(const std::string& plugin_name, const std::string& topic_name);
+        void stimulus_receive_json_message(const std::string& plugin_name, const std::string& topic_name, const std::string& json_message);
 
-		void setup_viewer(const std::string& viewer_window_name, const std::string& json_config, bool pbuffer=false);
-		void resized(const int& width, const int& height);
+        void setup_viewer(const std::string& viewer_window_name, const std::string& json_config, bool pbuffer=false);
+        void resized(const int& width, const int& height);
 
         void update( const double& time, const osg::Vec3& observer_position, const osg::Quat& observer_orientation );
-		void frame();
-		bool done();
+        void frame();
+        bool done();
 
-		int getXSize() {return _width;}
-		int getYSize() {return _height;}
+        int getXSize() {return _width;}
+        int getYSize() {return _height;}
 
-		float getFrameRate();
-		void setCursorVisible(bool visible);
-		void setWindowName(std::string name);
-		void setGamma(float gamma);
-		void setRedMax(bool red_max);
+        float getFrameRate();
+        void setCursorVisible(bool visible);
+        void setWindowName(std::string name);
+        void setGamma(float gamma);
+        void setRedMax(bool red_max);
 
-		void setCaptureImageFilename(std::string name);
-		void setCaptureOSGFilename(std::string name);
+        void setCaptureImageFilename(std::string name);
+        void setCaptureOSGFilename(std::string name);
 
         TrackballManipulatorState getTrackballManipulatorState();
         void setTrackballManipulatorState(TrackballManipulatorState s);
 
-	private:
-		StimulusLoader _stimulus_loader;
+    private:
+        StimulusLoader _stimulus_loader;
 
-		std::map<std::string, StimulusInterface*> _stimulus_plugins;
-		StimulusInterface* _current_stimulus;
-		std::string _mode;
+        std::map<std::string, StimulusInterface*> _stimulus_plugins;
+        StimulusInterface* _current_stimulus;
+        std::string _mode;
 
-		Poco::Path _flyvr_basepath;
-		Poco::Path _config_file_path;
+        Poco::Path _flyvr_basepath;
+        Poco::Path _config_file_path;
 
-		osgViewer::Viewer* _viewer;
-		osg::ref_ptr<osg::PositionAttitudeTransform> _observer_pat;
-		osg::ref_ptr<osg::PositionAttitudeTransform> _observer_geometry_pat;
-		ObserverPositionCallback* _observer_cb;
-		bool _tethered_mode;
+        osgViewer::Viewer* _viewer;
+        osg::ref_ptr<osg::PositionAttitudeTransform> _observer_pat;
+        osg::ref_ptr<osg::PositionAttitudeTransform> _observer_geometry_pat;
+        ObserverPositionCallback* _observer_cb;
+        bool _tethered_mode;
 
-		CameraCube* _cubemap_maker;
+        CameraCube* _cubemap_maker;
 
-		osg::ref_ptr<osg::Camera> _hud_cam;
-		osg::ref_ptr<osg::Group> _active_3d_world;
-		osg::ref_ptr<osg::Group> _active_2d_hud;
+        osg::ref_ptr<osg::Camera> _hud_cam;
+        osg::ref_ptr<osg::Group> _active_3d_world;
+        osg::ref_ptr<osg::Group> _active_2d_hud;
 
-		int _width;
-		int _height;
-		osg::ref_ptr<osgGA::TrackballManipulator> _cameraManipulator;
-		WindowCaptureCallback* _wcc;
+        int _width;
+        int _height;
+        osg::ref_ptr<osgGA::TrackballManipulator> _cameraManipulator;
+        WindowCaptureCallback* _wcc;
         GeometryTextureToDisplayImagePass *_g2di;
         OpenThreads::Mutex _osg_capture_mutex;
         std::string        _osg_capture_filename;
-	};
+    };
 
 }
 
