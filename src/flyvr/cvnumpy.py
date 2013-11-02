2e74add61ba9c66cccb142bbbb7545f4a6ee303a
Andrew Straw <strawman@astraw.com>
2013-11-02 16:08:35 +0100

    cvnumpy: remove unused code now that we switched to cv2

--- src/flyvr/cvnumpy.py
+++ src/flyvr/cvnumpy.py
@@ -1,41 +1,5 @@
 import numpy as np
-import cv
 import cv2
-
-def numpy2opencv_image(arr):
-    arr = np.array(arr)
-    assert arr.ndim==2
-    if arr.dtype in [np.float32]:
-        result = cv.CreateMat( arr.shape[0], arr.shape[1], cv.CV_32FC1)
-    elif arr.dtype in [np.float64, np.float]:
-        result = cv.CreateMat( arr.shape[0], arr.shape[1], cv.CV_64FC1)
-    elif arr.dtype in [np.uint8]:
-        result = cv.CreateMat( arr.shape[0], arr.shape[1], cv.CV_8UC1)
-    else:
-        raise ValueError('unknown numpy dtype "%s"'%arr.dtype)
-    for i in range(arr.shape[0]):
-        for j in range(arr.shape[1]):
-            result[i,j] = arr[i,j]
-    return result
-
-def opencv_image2numpy( cvimage ):
-    pyobj = np.asarray(cvimage)
-    if pyobj.ndim == 2:
-        # new OpenCV version
-        result = pyobj
-    else:
-        # old OpenCV, so hack this
-        width = cvimage.width
-        height = cvimage.height
-        assert cvimage.channels == 1
-        assert cvimage.nChannels == 1
-        assert cvimage.depth == 32
-        assert cvimage.origin == 0
-        result = np.empty( (height,width), dtype=np.float )
-        for i in range(height):
-            for j in range(width):
-                result[i,j] = cvimage[i,j]
-    return result
 
 def rodrigues2matrix_cv(params):
     rvec = np.array(params,dtype=np.float64)
