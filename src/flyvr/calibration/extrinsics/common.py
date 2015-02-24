3e9338ef2fd0ebac4cca66eeffb8c804c1efde3a
ap-- <mail@andreaspoehlmann.de>
2015-02-24 14:23:05 +0100

    WIP refactored cv2 PnP based extrinsic fitting module

---
+++ src/flyvr/calibration/extrinsics/common.py
@@ -0,0 +1,52 @@
+import numpy
+import pymvg
+
+def is_3d_point(point):
+    return point.ndim == 1 and point.shape == (3,)
+
+def is_3d_point_array(points):
+    return points.ndim == 2 and points.shape[1] == 3
+
+def is_2d_point(point):
+    return point.ndim == 1 and point.shape == (2,)
+
+def is_2d_point_array(points):
+    return points.ndim == 2 and points.shape[1] == 2
+
+def is_3d_vector(vec):
+    # TODO
+    return True
+
+def is_rodrigues(vec):
+    # TODO
+    return True
+
+
+def prepare_input(camera, points_3d, points_2d, extrinsics_guess):
+    """prepares the input data for all other fitting functions"""
+
+    assert is_3d_point_array(points_3d), "points_3d.shape != (N,3)"
+    assert is_2d_point_array(points_2d), "points_2d.shape != (N,2)"
+    assert points_3d.shape[0] == points_2d.shape[0], "len(points_3d) != len(points_2d)"
+
+    if extrinsics_guess is None:
+        USE_EXTRINSIC_GUESS = True
+    else:
+        # a guess for the camera position was provided!
+        try:
+            rvec, tvec = extrinsics_guess
+        except (ValueError, TypeError):
+            raise ValueError("extrinsics_guess should be [rvec, tvec]")
+
+        assert is_rodrigues(rvec), "rvec is not a rodrigues rotation vector"
+        assert is_3d_vector(tvec), "tvec is not a 3d vector"
+
+        USE_EXTRINSIC_GUESS = False
+
+    pts2d = numpy.array(points_2d, dtype=numpy.float64)
+    pts3d = numpy.array(points_3d, dtype=numpy.float64)
+    cam_matrix = numpy.array(camera.get_K(), dtype=numpy.float64)
+    cam_dist_coeffs = numpy.array(camera.get_D(), dtype=numpy.float64)
+    extguess = (rvec, tvec) if USE_EXTRINSIC_GUESS else tuple()
+
+    return pts3d, pts2d, cam_matrix, cam_dist_coeffs, extguess
---
+++ src/flyvr/calibration/extrinsics/pnp.py
@@ -0,0 +1,130 @@
+
+from .common import prepare_input
+import cv2
+
+def _cv2_solvepnp(camera, points_3d, points_2d, extrinsics_guess=None, flags=None, errmsg=""):
+    """helper for cv2.solvepnp"""
+    pts3d, pts2d, cam_matrix, cam_dist_coeffs, extguess = prepare_input(camera, points_3d, points_2d, extrinsics_guess)
+
+    # Try fitting the extrinsics
+    if not extguess:
+        retval, rvec, tvec = cv2.solvePnP(pts3d, pts2d, camera_matrix, camera_dist_coeffs, flags=flags)
+    else:
+        rvec, tvec = extguess
+        retval, rvec, tvec = cv2.solvePnP(pts3d, pts2d, camera_matrix, camera_dist_coeffs, rvec, tvec, 1, flags=flags)
+
+    if not retval:
+        raise RuntimeError("")
+    return rvec, tvec, None
+
+
+
+def pnp_iterative(camera, points_3d, points_2d, extrinsics_guess=None, params={}):
+    """Fits the extrinsic matrices for a camera using 3d and 2d correspondences
+
+    FIXME: parameters
+
+    """
+    return _cv2_solvepnp(camera, points_3d, points_2d, extrinsics_guess,
+                         flags=cv2.CV_ITERATIVE,
+                         errmsg="pnp_iterative did not succeed.")
+pnp_iterative.params = {}
+
+
+def pnp_p3p(camera, points_3d, points_2d, extrinsics_guess=None, params={}):
+    """Fits the extrinsic matrices for a camera using 3d and 2d correspondences
+
+    FIXME: parameters
+
+    """
+    return _cv2_solvepnp(camera, points_3d, points_2d, extrinsics_guess,
+                         flags=cv2.CV_P3P,
+                         errmsg="pnp_p3p did not succeed.")
+pnp_p3p.params = {}
+
+def pnp_epnp(camera, points_3d, points_2d, extrinsics_guess=None, params={}):
+    """Fits the extrinsic matrices for a camera using 3d and 2d correspondences
+
+    FIXME: parameters
+
+    """
+    return _cv2_solvepnp(camera, points_3d, points_2d, extrinsics_guess,
+                         flags=cv2.CV_EPNP,
+                         errmsg="pnp_epnp did not succeed.")
+pnp_epnp.params = {}
+
+
+
+def _cv2_solvepnpransac(camera, points_3d, points_2d, extrinsics_guess=None, params=None, flags=None, errmsg=""):
+    """helper for cv2.solvepnp"""
+    pts3d, pts2d, cam_matrix, cam_dist_coeffs, extguess = prepare_input(camera, points_3d, points_2d, extrinsics_guess)
+
+    iterations_count = params.get('iterations_count', 100)
+    reprojection_error = params.get('reprojection_error', 8.0)
+    min_inliers_count = params.get('min_inliers_count', 100)
+
+    # Try fitting the extrinsics
+    if not extguess:
+        rvec, tvec, inliers = cv2.solvePnPRansac(pts3d, pts2d, camera_matrix, camera_dist_coeffs,
+                                                 iterationsCount=iterations_count,
+                                                 reprojectionError=reprojection_error,
+                                                 minInliersCount=min_inliers_count,
+                                                 flags=flags)
+    else:
+        rvec, tvec = extguess
+        rvec, tvec, inliers = cv2.solvePnPRansac(pts3d, pts2d, camera_matrix, camera_dist_coeffs, rvec, tvec, 1,
+                                                 iterationsCount=iterations_count,
+                                                 reprojectionError=reprojection_error,
+                                                 minInliersCount=min_inliers_count,
+                                                 flags=flags)
+    return rvec, tvec, {'inliers': inliers}
+
+
+def pnp_ransac_iterative(camera, points_3d, points_2d, extrinsics_guess=None, params={}):
+    """Fits the extrinsic matrices for a camera using 3d and 2d correspondences
+
+    FIXME: parameters
+
+    """
+    return _cv2_solvepnpransac(camera, points_3d, points_2d, extrinsics_guess,
+                               params=params,
+                               flags=cv2.CV_ITERATIVE,
+                               errmsg="pnp_ransac_iterative did not succeed.")
+pnp_ransac_iterative.params = {
+        'iterations_count': int,
+        'reprojection_error': float,
+        'min_inliers_count': float
+        }
+
+
+def pnp_ransac_p3p(camera, points_3d, points_2d, extrinsics_guess=None, params={}):
+    """Fits the extrinsic matrices for a camera using 3d and 2d correspondences
+
+    FIXME: parameters
+
+    """
+    return _cv2_solvepnpransac(camera, points_3d, points_2d, extrinsics_guess,
+                               params=params,
+                               flags=cv2.CV_P3P,
+                               errmsg="pnp_ransac_p3p did not succeed.")
+pnp_ransac_p3p.params = {
+        'iterations_count': int,
+        'reprojection_error': float,
+        'min_inliers_count': float
+        }
+
+def pnp_ransac_epnp(camera, points_3d, points_2d, extrinsics_guess=None, params={}):
+    """Fits the extrinsic matrices for a camera using 3d and 2d correspondences
+
+    FIXME: parameters
+
+    """
+    return _cv2_solvepnpransac(camera, points_3d, points_2d, extrinsics_guess,
+                               params=params,
+                               flags=cv2.CV_EPNP,
+                               errmsg="pnp_ransac_epnp did not succeed.")
+pnp_ransac_epnp.params = {
+        'iterations_count': int,
+        'reprojection_error': float,
+        'min_inliers_count': float
+        }
