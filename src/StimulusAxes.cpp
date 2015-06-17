f4cac72673a8bd3ca6f8cc3ee528a03d646c63a1
John Stowers <john.stowers@gmail.com>
2015-06-17 19:13:19 +0200

    StimulusAxes: stimulus that always remains locked to observer
    draws three orthogonal colored planes (x,y,z) originating
    at the observer. should allow checking geometry models.

---
+++ src/StimulusAxes.cpp
@@ -0,0 +1,112 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+#include "flyvr/StimulusInterface.hpp"
+
+#include "Poco/ClassLibrary.h"
+
+#include <iostream>
+
+#include <osg/MatrixTransform>
+#include <osg/TextureCubeMap>
+#include <osg/ShapeDrawable>
+#include <osg/Material>
+#include <osg/CullFace>
+#include <osg/Light>
+#include <osg/LightSource>
+#include <osg/LightModel>
+
+#include <osgDB/ReadFile>
+#include <osgDB/WriteFile>
+#include <osgDB/FileUtils>
+
+class StimulusAxes: public StimulusInterface
+{
+public:
+std::string name() const {
+	return "StimulusAxes";
+}
+
+virtual void post_init(bool slave) {
+	_virtual_world = create_virtual_world();
+}
+
+void resized(int width,int height) {
+}
+
+void update( const double& time, const osg::Vec3& observer_position, const osg::Quat& observer_orientation ) {
+    _virtual_world->setPosition(observer_position);
+}
+
+osg::ref_ptr<osg::Group> get_3d_world() {
+    return _virtual_world;
+}
+
+osg::ref_ptr<osg::Group> get_2d_hud() {
+    return 0;
+}
+
+std::vector<std::string> get_topic_names() const {
+	std::vector<std::string> result;
+	return result;
+}
+
+void receive_json_message(const std::string& topic_name, const std::string& json_message) {
+}
+
+std::string get_message_type(const std::string& topic_name) const {
+	throw 0; // TODO: better error handling
+}
+
+osg::ref_ptr<osg::PositionAttitudeTransform> create_virtual_world()
+{
+  osg::ref_ptr<osg::PositionAttitudeTransform> myroot = new osg::PositionAttitudeTransform;
+  myroot->addDescription("virtual world root node");
+  //disable all lighting in the scene so objects always appear the same color.
+  myroot->getOrCreateStateSet()->setMode(GL_LIGHTING, osg::StateAttribute::OFF);
+
+  osg::ref_ptr<osg::Geode> geode = new osg::Geode;
+  myroot->addChild(geode.get());
+
+  osg::ref_ptr<osg::ShapeDrawable> shape;
+
+  float L = 0.5f;
+  float l = 0.005f;
+  osg::Vec3 center = osg::Vec3(0.0f, 0.0f, 0.0f);
+
+  _plane_x = new osg::Box(center,l,L,L);
+  shape = new osg::ShapeDrawable(_plane_x);
+  shape->setColor(osg::Vec4(1.0f, 0.0f, 0.0f, 1.0f));
+  geode->addDrawable(shape.get());
+
+  _plane_y = new osg::Box(center,L,l,L);
+  shape = new osg::ShapeDrawable(_plane_y);
+  shape->setColor(osg::Vec4(0.0f, 1.0f, 0.0f, 1.0f));
+  geode->addDrawable(shape.get());
+
+  _plane_z = new osg::Box(center,L,L,l);
+  shape = new osg::ShapeDrawable(_plane_z);
+  shape->setColor(osg::Vec4(0.0f, 0.0f, 1.0f, 1.0f));
+  geode->addDrawable(shape.get());
+
+  return myroot;
+}
+
+private:
+    osg::ref_ptr<osg::Geode> _geode;
+    osg::ref_ptr<osg::PositionAttitudeTransform>            _virtual_world;
+    osg::ref_ptr<osg::Box> _plane_x;
+    osg::ref_ptr<osg::Box> _plane_y;
+    osg::ref_ptr<osg::Box> _plane_z;
+};
+
+
+POCO_BEGIN_MANIFEST(StimulusInterface)
+POCO_EXPORT_CLASS(StimulusAxes)
+POCO_END_MANIFEST
+
+void pocoInitializeLibrary()
+{
+}
+
+void pocoUninitializeLibrary()
+{
+}
