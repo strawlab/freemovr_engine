ed45ca2540804579663b2878a9f5147d24f22d82
John Stowers <john.stowers@gmail.com>
2016-05-16 20:14:18 +0200

    flyvr.osg_utils: move more stuff here

--- src/flyvr/osg_utils.py
+++ src/flyvr/osg_utils.py
@@ -2,11 +2,12 @@ import subprocess
 
 import roslib
 roslib.load_manifest('flyvr')
+import flyvr.display_client as display_client
 
 import geometry_msgs.msg
 import std_msgs.msg
 
-def build_move_node_message(name, x=0, y=0, z=0, scale=1, orientation_x=0, orientation_y=0, orientation_z=0):
+def build_move_node_message(name, x=0, y=0, z=0, scale=1, hidden=False, orientation_x=0, orientation_y=0, orientation_z=0):
     p = geometry_msgs.msg.PoseStamped()
     p.header.frame_id = name
     p.pose.position.x = x
@@ -14,7 +15,7 @@ def build_move_node_message(name, x=0, y=0, z=0, scale=1, orientation_x=0, orien
     p.pose.position.z = z
     # w is interpreted as the scale of the object in the OSG file. 0 scale
     # hides it, for example
-    p.pose.orientation.w = scale
+    p.pose.orientation.w = 0 if hidden else scale
     p.pose.orientation.x = orientation_x
     p.pose.orientation.y = orientation_y
     p.pose.orientation.z = orientation_z
@@ -37,7 +38,35 @@ def parse_osg_file(name):
     return nodes, animations
 
 class StimulusOSGController(object):
-    pass
+
+    def __init__(self):
+        self.pub_filename = rospy.Publisher('osg_filename', std_msgs.msg.String, latch=True)
+        self.pub_submodel = rospy.Publisher('osg_submodel_pose', geometry_msgs.msg.PoseStamped)
+        self.pub_anim_start = rospy.Publisher('osg_animation_start', std_msgs.msg.String, latch=True)
+        self.pub_anim_stop = rospy.Publisher('osg_animation_stop', std_msgs.msg.String, latch=True)
+
+        display_client.DisplayServerProxy.set_stimulus_mode('StimulusOSG')
+
+    @staticmethod
+    def get_animations(path):
+        return parse_osg_file(path)[1]
+
+    @staticmethod
+    def get_nodes(path):
+        return parse_osg_file(path)[0]
+
+    def load_osg(self, path):
+        self.pub_filename.publish(path)
+
+    def animation_start(self, name):
+        self.pub_anim_start.publish(name)
+
+    def animation_stop(self, name):
+        self.pub_anim_stop.publish(name)
+
+    def move_node(self, *args, **kwargs):
+        msg = build_move_node_message(*args, **kwargs)
+        self.pub_submodel.publish(msg)
 
 
 if __name__ == "__main__":
@@ -45,3 +74,4 @@ if __name__ == "__main__":
     n,a = parse_osg_file(sys.argv[1])
     print "nodes:",",".join(n)
     print "animations:",",".join(a)
+
