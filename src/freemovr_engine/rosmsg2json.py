3eeb587415f06372f85fbdda7d861a85ed631e8d
Andrew Straw <strawman@astraw.com>
2017-05-06 23:48:02 +0200

    unify package name specification
    This updates to partial compatibility with motmot_ros_utils package,
    specifically the URL format decoded by rosutils.io.decode_url() starting
    with 'package://' which is the form we use most.

--- src/freemovr_engine/rosmsg2json.py
+++ src/freemovr_engine/rosmsg2json.py
@@ -1,6 +1,8 @@
 import json
 import base64
 import re
+import os
+import warnings
 
 import roslib.packages
 roslib.load_manifest('freemovr_engine')
@@ -24,17 +26,34 @@ else:
     time_base_type = roslib.rostime.Time
 
 # globals
-re_ros_path = re.compile(r'\$\(find (.*)\)')
+re_ros_path = re.compile(r'package://(.*?)/')
+re_ros_path_old = re.compile(r'\$\(find (.*)\)') # deprecated in favor of above
 re_array = re.compile(r'(.+)\[(\d*)\]$')
 
 BASIC_TYPES = ['float64','float32','string','uint32','uint8','bool','int8','int32']
 
 def _findrepl(matchobj):
     ros_pkg_name = matchobj.group(1)
-    return roslib.packages.get_pkg_dir(ros_pkg_name)
+    return roslib.packages.get_pkg_dir(ros_pkg_name) + os.sep
+
+def _suggestrepl(matchobj):
+    ros_pkg_name = matchobj.group(1)
+    return 'package://%s' % ros_pkg_name
 
 def fixup_path( orig_path ):
-    return re_ros_path.sub( _findrepl, orig_path )
+    # Ideally this would use the motmot_ros_utils package,
+    # specifically the URL format decoded by rosutils.io.decode_url()
+    fixed_path = re_ros_path.sub( _findrepl, orig_path )
+    if fixed_path != orig_path:
+        return fixed_path
+    fixed_path = re_ros_path_old.sub( _findrepl, orig_path )
+    if fixed_path != orig_path:
+        new_fixed = re_ros_path_old.sub( _suggestrepl, orig_path )
+        warnings.warn("a path was specified with %r but "
+                      "this is deprecated and should be %r" % (
+                      fixed_path, new_fixed))
+        return fixed_path
+    return orig_path
 
 def convert_attrs( v ):
     return dict( (attr,getattr(v,attr)) for attr in v.__slots__)
