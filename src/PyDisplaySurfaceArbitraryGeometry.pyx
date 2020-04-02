7480cc2049c0732c7acd62b157fab9870027b6a7
Andrew Straw <strawman@astraw.com>
2020-04-02 14:39:07 +0200

    python3 strings are automatically converted to std::string
    Also add Cython pragmas

--- src/PyDisplaySurfaceArbitraryGeometry.pyx
+++ src/PyDisplaySurfaceArbitraryGeometry.pyx
@@ -1,3 +1,8 @@
+# distutils: language=c++
+# distutils: sources = src/DisplaySurfaceGeometry.cpp src/DisplaySurfaceArbitraryGeometry.cpp
+# distutils: language_level: 2
+# cython: c_string_type=unicode, c_string_encoding=utf8
+
 from libcpp.string cimport string
 import freemovr_engine
 import sys
