902b323920759620844c3831f4753afc5c1edaab
Andrew Straw <strawman@astraw.com>
2020-06-06 08:33:12 +0200

    convert readme to markdown

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ from Cython.Build import cythonize
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='freemovr_engine',
     description='the FreemoVR virtual reality engine',
-    long_description_content_type='text/x-rst',
+    long_description_content_type='text/markdown',
     url='https://github.com/strawlab/freemovr_engine',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
