d2aa115517b585a07f5e0efac232299ca0c18be6
Andrew Straw <strawman@astraw.com>
2022-01-16 12:33:12 +0100

    noetic python3 fixes

--- src/freemovr_engine/fixup_path.py
+++ src/freemovr_engine/fixup_path.py
@@ -1,4 +1,5 @@
-import re
+import re, os, warnings
+import roslib
 
 # globals
 re_ros_path = re.compile(r'package://(.*?)/')
@@ -7,6 +8,10 @@ re_ros_path_old = re.compile(r'\$\(find (.*)\)') # deprecated in favor of above
 def _findrepl(matchobj):
     ros_pkg_name = matchobj.group(1)
     return roslib.packages.get_pkg_dir(ros_pkg_name) + os.sep
+
+def _suggestrepl(matchobj):
+    ros_pkg_name = matchobj.group(1)
+    return 'package://%s' % ros_pkg_name
 
 def fixup_path( orig_path ):
     # Ideally this would use the motmot_ros_utils package,
@@ -19,6 +24,6 @@ def fixup_path( orig_path ):
         new_fixed = re_ros_path_old.sub( _suggestrepl, orig_path )
         warnings.warn("a path was specified with %r but "
                       "this is deprecated and should be %r" % (
-                      fixed_path, new_fixed))
+                      orig_path, new_fixed))
         return fixed_path
     return orig_path
